ac3b0025f55c9d631783647f92ed52ed06622b32
Saiful Islam <saiful70.me@gmail.com>
2021-12-12 23:26:09 +0600

    day-37 december-12,2021

--- 048. house-robber.js
+++ 048. house-robber.js
@@ -7,7 +7,7 @@ const rob = (nums) => {
     dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
   }
 
-  return dp[nums.length - 1];
+  return dp[nums.length - 1]; 
 };
 
 console.log(rob([2, 7, 9, 3, 1]));
