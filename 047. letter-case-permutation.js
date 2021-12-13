5331ddf3d887d68fdd475121c5fb91ff48beebc5
Saiful Islam <saiful70.me@gmail.com>
2021-12-13 11:14:09 +0600

    day-38 december-13,2021

--- 047. letter-case-permutation.js
+++ 047. letter-case-permutation.js
@@ -1,37 +1,31 @@
 const letterCasePermutation = (s) => {
-  s = s.toLowerCase();
+  let len = s.length;
   let res = [];
 
-  res.push(s);
-  res.push(s.toUpperCase());
+  let tempArr = [];
 
-  for (let i = 0; i < s.length; i++) {
-    let str = [],
-      str2 = [];
-    if (s[i] > "a" && s[i] < "z") {
-      for (let j = 0; j < s.length; j++) {
-        if (i === j) {
-          str.push(s[j]);
-          str2.push(s[j].toUpperCase());
-        } else {
-          str.push(s[j].toUpperCase());
-          str2.push(s[j].toLowerCase());
-        }
-      }
-
-      res.push(str.join(""));
-      res.push(str2.join(""));
+  const backTrack = (i) => {
+    if (i === len) {
+      return res.push(tempArr.join(""));
     }
-  }
 
-  const onlyUnique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  }
+    if (s[i] >= "A") {
+      tempArr[i] = s[i].toLowerCase();
+      backTrack(i + 1);
+      tempArr[i] = s[i].toUpperCase();
+      backTrack(i + 1);
+    } else {
+      tempArr[i] = s[i];
+      backTrack(i + 1);
+    }
+  };
 
-  return res.filter(onlyUnique);
+  
+  backTrack(0);
+  return res;
 };
 
-// console.log(letterCasePermutation("a1b2"));
+console.log(letterCasePermutation("a1b2"));
 // console.log(letterCasePermutation("0"));
 // console.log(letterCasePermutation("12345"));
-console.log(letterCasePermutation("dnTJ"));
+// console.log(letterCasePermutation("dnTJ"));
--- 049. triangle.js
+++ 049. triangle.js
@@ -3,42 +3,16 @@ const minimumTotal = (triangle) => {
     return triangle[0][0];
   }
 
-  let i,
-    j,
-    index = 1,
-    len,
-    mn = Infinity;
-
-  let res = triangle[0][0];
-  for (i = 1; i < triangle.length; i++) {
-    // console.log(index);
-    len = triangle[i].length;
-
-    if (index === 0) {
-      j = index;
-    } else {
-      j = index-1;
+  for (let i = triangle.length - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1]);
     }
-
-    let val = index;
-    // console.log(val);
-    for (; j <= val + 1 && j < len; j++) {
-      if (triangle[i][j] < mn) {
-        mn = triangle[i][j];
-        index = j;
-      }
-    }
-    console.log(res+= mn);
-    res+= mn;
-
-    mn = Infinity;
-    // console.log(res);
   }
 
-  // return res
+  return triangle[0][0];
 };
 
-// console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]));
-// console.log(minimumTotal([[2], [3, -4]]));
-// console.log(minimumTotal([[-10]]));
+console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]));
+console.log(minimumTotal([[2], [3, -4]]));
+console.log(minimumTotal([[-10]]));
 console.log(minimumTotal([[-1], [2, 3], [1, -1, -3]]));
